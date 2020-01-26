a851cc84f52d7c8c26dfd390bd201aff2341ff2d
harsh242 <49694914+harsh242@users.noreply.github.com>
2020-01-26 18:30:58 +0530

    Add files via upload

--- UTKANSH FINAL/js/main.js
+++ UTKANSH FINAL/js/main.js
@@ -83,3 +83,23 @@ jQuery(document).ready(function($){
 		}, 0);
 	}
 });
+
+
+	/* Preloader 
+	 * -------------------------------------------------- */
+	var ssPreloader = function() {
+
+		$WIN.on('load', function() {	
+
+			// force page scroll position to top at page refresh
+			$('html, body').animate({ scrollTop: 0 }, 'normal');
+
+	      // will first fade out the loading animation 
+	    	$("#loader").fadeOut("slow", function(){
+
+	        // will fade out the whole DIV that covers the website.
+	        $("#preloader").delay(300).fadeOut("slow");
+
+	      }); 
+	  	});
+	}; 
